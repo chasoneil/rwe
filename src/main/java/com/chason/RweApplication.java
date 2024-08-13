4879b8d95a84688888d806ee5bf417f1ba35c173
chason <chason001@126.com>
2024-08-13 17:22:50 +0800

    fix some problem

--- src/main/java/com/chason/RweApplication.java
+++ src/main/java/com/chason/RweApplication.java
@@ -1,6 +1,7 @@
 package com.chason;
 
 import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -15,6 +16,6 @@ public class RweApplication
     public static void main(String[] args)
     {
         SpringApplication.run(RweApplication.class, args);
-        System.out.println("ヾ(◍°∇°◍)ﾉﾞ   小乙网控管家启动成功      ヾ(◍°∇°◍)ﾉﾞ\n");
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ   背单词启动成功     ヾ(◍°∇°◍)ﾉﾞ\n");
     }
 }
