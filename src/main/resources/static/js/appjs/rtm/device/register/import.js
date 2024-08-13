065d8bacbff39337ee8a363ef3e2fadbd5da1e6c
chason <chason001@126.com>
2024-08-13 16:45:55 +0800

    20240813 git init for add dir static  7

---
+++ src/main/resources/static/js/appjs/rtm/device/register/import.js
@@ -0,0 +1,55 @@
+//导入设备信息文件
+$().ready(function() {
+	validateRule();
+});
+
+$.validator.setDefaults({
+	submitHandler : function() {
+		save();
+	}
+});
+
+function save() {
+	//var formData = new FormData(document.forms.namedItem("signupForm"));
+	var formData = new FormData($('#signupForm')[0]);
+	$.ajax({
+		type: 'POST',
+        url: "/rwe/device/register/importDo",
+        data:formData,
+        async: false,
+        cache: false,
+        contentType: false,
+        processData: false,
+		error : function(request) {
+			parent.layer.alert("Connection error");
+		},
+		success : function(data) {
+			if (data.code == 0) {
+				parent.layer.msg("操作成功");
+				parent.reLoad();
+				var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+				parent.layer.close(index);
+
+			} else {
+				parent.layer.msg(data.msg)
+			}
+
+		}
+	});
+}
+
+function validateRule() {
+	var icon = "<i class='fa fa-times-circle'></i> ";
+	$("#signupForm").validate({
+		rules : {
+			files : {
+				required : true
+			}
+		},
+		messages : {
+			files : {
+				required : icon + "请选择文件"
+			}
+		}
+	})
+}
