e0e2901d0a1c6543a6d360761c3734a512441338
chasoneil <chason001@126.com>
2025-11-07 17:38:48 +0800

    修复：动画异常的问题

--- src/main/resources/static/js/appjs/rwe/jp/rem/index.js
+++ src/main/resources/static/js/appjs/rwe/jp/rem/index.js
@@ -1,10 +1,21 @@
 const prefix = "/rwe/jp/rem";
 
 $(function () {
+    $('.contact-box').css({
+        transition: 'transform 0.5s ease-in-out' // 延长动画时间为1.5秒
+    });
 
+    $('.contact-box').hover(
+        function () {
+            // 鼠标进入时放大元素
+            $(this).css('transform', 'scale(1.1)');
+        },
+        function () {
+            // 鼠标离开时恢复原始大小
+            $(this).css('transform', 'scale(1)');
+        }
+    );
 });
-
-
 
 function add() {
     layer.open({
