3fe7feba8d5fa86b9a1b90556b87b38ac1e06970
chasoneil <chason001@126.com>
2025-12-02 17:58:02 +0800

    更新sentence

--- src/main/resources/static/js/appjs/rwe/jp/sentence/sentence.js
+++ src/main/resources/static/js/appjs/rwe/jp/sentence/sentence.js
@@ -21,17 +21,15 @@ function loadData() {
         },
         success : function(data) {
             if (data.code == 0) {
-                sentences = JSON.parse(data.msg);
-                length = sentenceArray.length;
+                let sentences = JSON.parse(data.msg);
 
-                let singles = sentences.singles();
-                let dialogs = sentences.dialogs();
+                let singles = sentences.singles;
+                let dialogs = sentences.dialogs;
 
                 // 将数组中的数据打乱
                 singles.sort(() => Math.random() - 0.5);
                 dialogs.sort(() => Math.random() - 0.5);
                 
-                console.log(dialogs);
                 initSingle(singles);
                 initDialog(dialogs);
 
@@ -49,29 +47,62 @@ function initSingle(singles) {
     let title = "";
     for (let index = 0; index < singles.length; index++) {
         let single = singles[index];
-        console.log(single);
-        title = single.title();
-        let content = single.content();
+        title = single.title;
+        let content = single.content;
         let split = content.split("-");
         let cn = split[0];
         let jp = split[1];
-        setSingle(cn, jp)
+        setSingle(cn, jp, index)
     }
 }
 
 function initDialog(dialogs) {
+    let title = "";
+    for (let index = 0; index < dialogs.length; index++) {
+        let dialog = dialogs[index];
+        console.log(dialog);
+        title = dialog.title;
+        // let content = dialog.content;
+        // let split = content.split("-");
+        // let cn = split[0];
+        // let jp = split[1];
+        // setSingle(cn, jp, index)
+    }
+}
+
+function setSingle(cn, jp, index) {
+
+    let elements = `
+        <h3>${cn}</h3>
+        <div class="input-group" style="display: flex; align-items: center;">
+            <input type="hidden" class="form-control" value="${jp}" id="jpa-${index}">
+            <input type="text" class="form-control" placeholder="请将上面的中文翻译成日语" id="jpu-${index}">
+            <button type="button" class="btn btn-sm btn-success" onclick="check('${index}')" style="margin-left:5px;margin-top:2px;">
+                <i class="fa fa-check" aria-hidden="true"></i> 检查
+            </button>
+        </div>    
+        <hr>
+    `;
+
+    $('#content').append(elements);
 
 }
 
-function setSingle() {
-
-}
-
-
-
-function passed() {
+function check(index) {
     
+    let resId = 'jpu-' + index;
+    let ansId = 'jpa-' + index;
+
+    let Q = $('#' + resId).val();
+    let A = $('#' + ansId).val();
+
+    if (Q === A) {
+        layer.msg('回答正确');
+    } else {
+        layer.alert('回答错误，正确答案:' + A);
+    }
 }
+
 
 
 
